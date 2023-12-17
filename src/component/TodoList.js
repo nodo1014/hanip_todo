7ba0b0d5c651bd896f336cc023efa34bfc03d49f
nodo1014 <htmsejong@gmail.com>
2023-12-17 14:44:38 +0900

    todoList 에 useMemo 사용. 검색시 렌더링 X

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 
 export default function TodoList({todo, onUpdate, onDelete}) {
-    const analyzeTodo = () => {
+    const analyzeTodo = useMemo(() => {
+        console.log('analyze 함수 호출 횟수-useMemo')
         const totalCount = todo.length;
         const isDoneCount = todo.filter((item)=>item.isDone).length
         const ingCount = totalCount - isDoneCount;
@@ -13,8 +14,12 @@ export default function TodoList({todo, onUpdate, onDelete}) {
             isDoneCount,
             ingCount,
         };
-    };
-    const {totalCount, isDoneCount, ingCount} = analyzeTodo();
+    },[todo]);
+    // const {totalCount, isDoneCount, ingCount} = analyzeTodo();
+    // useMemo는 함수가 아닌 값을 반환하므로, analyzeTodo 에는 값이 저장된다. 따라서 () 을 삭제.
+    const {totalCount, isDoneCount, ingCount} = analyzeTodo;
+    // 검색어를 입력해도, todo 값은 변하지 않았기 때문에, analyzeTodo 를 호출하지 않음.
+
     const [search, setSearch] = useState('');
     const onChangeSearch = (e) => {
         console.log(search);
